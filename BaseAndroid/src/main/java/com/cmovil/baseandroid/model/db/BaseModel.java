d5260c4c52960bf7a4c2721427a32009908533cf
Javier Silva <javier.silva@conceptomovil.com>
2013-09-20 13:25:17 -0500

    Convert BaseModel into abstract class do id could be added as attribute

--- BaseAndroid/src/main/java/com/cmovil/baseandroid/model/db/BaseModel.java
+++ BaseAndroid/src/main/java/com/cmovil/baseandroid/model/db/BaseModel.java
@@ -7,6 +7,8 @@
  */
 package com.cmovil.baseandroid.model.db;
 
+import com.cmovil.baseandroid.util.KeyDictionary;
+
 /**
  * Public abstract class that should be implemented by all the models of the application
  *
@@ -14,7 +16,25 @@ package com.cmovil.baseandroid.model.db;
  * @version 1.0
  * @since 19/08/13
  */
-public interface BaseModel {
+public abstract class BaseModel {
+
+	protected Integer id;
+
+	/**
+	 * Initialize attributes with default values
+	 */
+	public BaseModel() {
+		id = KeyDictionary.EMPTY_OBJECT_ID;
+	}
+
+	/**
+	 * Base constructor with id
+	 *
+	 * @param id Model row id
+	 */
+	protected BaseModel(Integer id) {
+		this.id = id;
+	}
 
 	/**
 	 * Return the description that will be shown in the view for these element
@@ -23,6 +43,12 @@ public interface BaseModel {
 	 */
 	public abstract String getShownDescription();
 
-	public Integer getId();
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
 }
